8601246dace7a752f4865b0aec9088d453a8c0ab
Leonardo Romo Avila <leonidas-45@hotmail.com>
2024-06-08 11:27:15 -0600

    adding sckeletor

---
+++ AuthenticationService/src/test/java/com/metaphorce/exam/ecommerce/ExamECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce.exam.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
