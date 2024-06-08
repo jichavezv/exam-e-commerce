61058410e68350929fe4f6dc5f7f9868f1e5fec4
jichavezv <chavezvelajuanignacio@gmail.com>
2024-06-07 18:14:00 -0600

    [FEATURE] Initial Commit

---
+++ src/main/java/com/metaphorce/exam/ecommerce/ExamECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.metaphorce.exam.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamECommerceApplication.class, args);
+	}
+
+}
